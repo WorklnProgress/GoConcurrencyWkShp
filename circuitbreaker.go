4373aa2a597b9607451406a59fcb6bdccaf02507
WorkInProgress <priya.sawant@gmail.com>
2022-11-04 07:05:33 -0400

    Update circuitbreaker.go

--- circuitbreaker.go
+++ circuitbreaker.go
@@ -1,8 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"time"
 )
+
+func main() {
+	cb := newCircuitBreaker(breakLimit)
+	concurrent := 10
+	ch := make(chan string)
+	for i := 0; i < concurrent; i++ {
+		// time.Sleep(time.Millisecond * 1)
+		go makeConcurrentRequest(cb, ch)
+	}
+	for i := 0; i < concurrent; i++ {
+		fmt.Println(<-ch)
+	}
+}
+
+// Provide a limit after which the circuit breaker trips
+const breakLimit = 5
+
+type sqlConnection interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+}
+
+type sqlDB struct{}
+
+// Fake Query
+func (db *sqlDB) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	time.Sleep(time.Millisecond * 10)
+	return nil, nil
+}
+
+// Simulate concurrent DB requests
+func makeConcurrentRequest(cb *circuitBreaker, ch chan<- string) {
+	start := time.Now()
+	_, err := cb.Execute()
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2f elapsed with err: %s", secs, err)
+}
 
 type circuitBreaker struct {
 	sqlDB
@@ -19,6 +57,8 @@ func newCircuitBreaker(limit int) *circuitBreaker {
 	return &circuitBreaker{sqlDB: s, queue: q, numCalled: 0}
 }
 
+// Execute will execute the db operation as long as
+// the CircuitBreaker is not over capacity
 func (cb *circuitBreaker) Execute() (rows interface{}, err error) {
 	cb.numCalled++
 	if !cb.canExecute() {
